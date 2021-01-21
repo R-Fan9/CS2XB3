4804ed95b5e2a7881436328ed8d2f5af1c58fce4
Eugene Fedoriv <fedorivy@mcmaster.ca>
2021-01-20 17:27:33 -0700

    implemented are_valid_groups function

--- code.py
+++ code.py
@@ -1,12 +1,22 @@
 def are_valid_groups(student_numbers, groups):
-    for student in student_numbers:
-        has_a_group=False
-        for group in groups:
-            if student in group:
-                has_a_group=True
-                break 
-        if not has_a_group:
-            return
-    return True
 
-    
+    if(len(set(student_numbers)) != len(student_numbers)):
+        return False
+
+    for student in student_numbers:
+        already_in_a_group = False
+        valid = False
+        if(type(student) is not type("")):
+            return False
+        for group in groups:
+            if(len(group) > 3 or len(group) < 2):
+                break
+            if student in group and not already_in_a_group:
+                valid = True
+                already_in_a_group = True 
+            elif student in group and already_in_a_group:
+                valid = False
+                break
+        if not valid:
+            break
+    return valid
