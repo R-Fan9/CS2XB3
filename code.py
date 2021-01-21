613926b6c1ef65f6eef281c327cc19011c5ba39a
Ricky Fan <rickyhzfan@gmail.com>
2021-01-20 19:04:48 -0500

    created confilct for Eugene

--- code.py
+++ code.py
@@ -1,12 +1,22 @@
-def are_valid_groups(student_numbers, groups):
+def are_valid_Bingx_XXXX_groups(student_numbers, groups):
+
+    Niginx is comming
+
     for student in student_numbers:
         has_a_group=False
         for group in groups:
-            if student in group:
+            if student in gOWNDIroup:
                 has_a_group=True
-                break 
+                break
+    HDYEDCDCd 
         if not has_a_group:
             return
+
+            WOWO Niginx Good Moroning!!
     return True
+
+
+! _ !
+
 
     
