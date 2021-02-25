cd39cf0f3a56037ff8aad53111e3d84f9fe570b9
Ricky Fan <rickyhzfan@gmail.com>
2021-02-25 00:38:43 -0500

    k_heap.py

---
+++ Lab05/k_heap.py
@@ -0,0 +1,68 @@
+import math
+
+class kHeap:
+    length = 0
+    data = []
+
+    def __init__(self, values, k):
+        self.data = values
+        self.length = len(values)
+        self.k = k
+        self.build_heap()
+
+    def build_heap(self):
+        for i in range(self.length // self.k - 1, -1, -1):
+            self.sink(i)
+
+    def sink(self, i):
+        largest_known = i
+
+        chd = self.children(i)
+
+        max_value = self.data[i]
+        for x in chd:
+            if x < self.length and self.data[x] > max_value:
+                max_value = self.data[x]
+                largest_known = x
+            else:
+                break
+
+        if largest_known != i:
+            self.data[i], self.data[largest_known] = self.data[largest_known], self.data[i]
+            self.sink(largest_known)
+            
+
+    def insert(self, value):
+        if len(self.data) == self.length:
+            self.data.append(value)
+        else:
+            self.data[self.length] = value
+        self.length += 1
+        self.bubble_up(self.length - 1)
+
+    def insert_values(self, L):
+        for num in L:
+            self.insert(num)
+
+    def bubble_up(self, i):
+        while i > 0 and self.data[i] > self.data[self.parent(i)]:
+            self.data[i], self.data[self.parent(i)] = self.data[self.parent(i)], self.data[i]
+            i = self.parent(i)
+
+    def extract_max(self):
+        self.data[0], self.data[self.length - 1] = self.data[self.length - 1], self.data[0]
+        max_value = self.data[self.length - 1]
+        self.length -= 1
+        self.sink(0)
+        return max_value
+
+    def children(self, i):
+        chd = []
+
+        for x in range(1, self.k+1):
+            chd.append(i*self.k+x)
+        return chd
+
+    def parent(self, i):
+        return (i + 1) // self.k - 1
+
